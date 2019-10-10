a9feb3716f2665c6ab45d670c52945f836c6f929
cfn1234 <435450916@qq.com>
2019-10-10 10:29:06 +0800

    java8 stream流例子

---
+++ src/main/java/com/zero/test/java8/Transaction.java
@@ -0,0 +1,35 @@
+package com.zero.test.java8;
+
+/**
+ * com.zero.test.java8
+ *
+ * @author caofengnian
+ * @Date 2019-09-29
+ */
+public class Transaction {
+    private final int id;
+    private final Integer value;
+    private final Type type;
+
+    public Transaction(int id, Integer value, Type type) {
+        this.id = id;
+        this.value = value;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public enum Type {
+        A, B, C, D, GEOCERY
+    }
+}
