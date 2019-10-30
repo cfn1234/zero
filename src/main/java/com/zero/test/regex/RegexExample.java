76309b64c0475852d1fd73942be00dfb92e370dc
cfn1234 <435450916@qq.com>
2019-10-30 15:12:09 +0800

    正则表达式

---
+++ src/main/java/com/zero/test/regex/RegexExample.java
@@ -0,0 +1,28 @@
+package com.zero.test.regex;
+
+import java.util.regex.Pattern;
+
+/**
+ * com.zero.test.regex
+ * 正则表达式
+ *
+ * @author caofengnian
+ * @Date 2019-10-28
+ */
+public class RegexExample {
+    public static void main(String[] args) {
+        int cnt =0;
+        boolean isMatch2 = Pattern.matches("^(?=.*[~!@#$%^&*()_+`\\-={}:\";'<>?,.\\/]).{6,20}$","11" );
+        boolean isMatch1 = Pattern.matches("^^(?=.*[a-zA-Z]).{6,20}$","11" );
+        boolean isMatch3 = Pattern.matches("^(?=.*\\d).{6,20}$","11" );
+        if(isMatch1){
+                ++cnt;
+        } if(isMatch2){
+            ++cnt;
+        } if(isMatch3){
+            ++cnt;
+        }
+
+        System.out.println(cnt);
+    }
+}
