2bdf13b7c092e8e190e94ca151ff725fe0badaf0
caofengnian <435450916@qq.com>
2021-09-17 17:58:53 +0800

    测试

--- src/main/java/com/zero/genericity/Test.java
+++ src/main/java/com/zero/genericity/Test.java
@@ -1,5 +1,8 @@
 package com.zero.genericity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * com.zero.genericity
  *
@@ -7,23 +10,32 @@ package com.zero.genericity;
  * @Date 2019-12-16
  */
 public class Test {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        GenericFruit.Apple apple = new GenericFruit.Apple();
-        GenericFruit.Person person = new GenericFruit.Person();
+		GenericFruit.Apple apple = new GenericFruit.Apple();
+		GenericFruit.Person person = new GenericFruit.Person();
+		List<GenericFruit.Apple> list = new ArrayList<>();
+		getList(list);
+		GenericFruit.GenerateTest<GenericFruit.Fruit> generateTest = new GenericFruit.GenerateTest<>();
+		//apple是Fruit的子类，所以这里可以
+		generateTest.show_1(apple);
+		//编译器会报错，因为泛型类型实参指定的是Fruit，而传入的实参类是Person
+		//generateTest.show_1(person);
 
-        GenericFruit.GenerateTest<GenericFruit.Fruit> generateTest = new GenericFruit.GenerateTest<>();
-        //apple是Fruit的子类，所以这里可以
-        generateTest.show_1(apple);
-        //编译器会报错，因为泛型类型实参指定的是Fruit，而传入的实参类是Person
-        //generateTest.show_1(person);
+		//使用这两个方法都可以成功
+		generateTest.show_2(apple);
+		generateTest.show_2(person);
 
-        //使用这两个方法都可以成功
-        generateTest.show_2(apple);
-        generateTest.show_2(person);
+		//使用这两个方法也都可以成功
+		generateTest.show_3(apple);
+		generateTest.show_3(person);
+	}
 
-        //使用这两个方法也都可以成功
-        generateTest.show_3(apple);
-        generateTest.show_3(person);
-    }
+	public static <T extends GenericFruit.Fruit> void getList(List<T> list) {
+		String a = "Hello";
+		// \u000d a="world";
+		System.out.println(a);
+		// \u000a a="hello world!";
+		System.out.println(a);
+	}
 }
--- src/main/java/com/zero/thread/synchronizedtest/Demo1.java
+++ src/main/java/com/zero/thread/synchronizedtest/Demo1.java
@@ -1,5 +1,7 @@
 package com.zero.thread.synchronizedtest;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 同步锁
  *
@@ -7,19 +9,63 @@ package com.zero.thread.synchronizedtest;
  * @date 2020-05-14
  */
 public class Demo1 {
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws InterruptedException {
 		Demo1 demo1 = new Demo1();
 		demo1.functionA();
+		new Thread(() -> {
+			synchronized (demo1) {
+				System.out.println("a");
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		TimeUnit.MILLISECONDS.sleep(10);
+		new Thread(() -> {
+			Thread.onSpinWait();
+			synchronized (demo1) {
+				System.out.println("b");
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		TimeUnit.MILLISECONDS.sleep(10);
+		new Thread(() -> {
+			synchronized (demo1) {
+				System.out.println("c");
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		TimeUnit.MILLISECONDS.sleep(10);
+		new Thread(() -> {
+			synchronized (demo1) {
+				System.out.println("d");
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
 	}
-	
-	public synchronized void functionA() {
+
+	public void functionA() {
 		System.out.println("iAmFunctionA");
 		functionB();
 	}
-	
+
 	public synchronized void functionB() {
 		System.out.println("iAmFunctionB");
 	}
-	
+
 }
